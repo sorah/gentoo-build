beb033eb84deb16040276ede4665717a2899af5d
Shota Fukumori (sora_h) <sorah@cookpad.com>
2015-05-06 06:52:42 +0900

    minify: OS X compat

--- minify.sh
+++ minify.sh
@@ -18,4 +18,8 @@ EOF
 
 chmod +x $output
 
-tar cjf - --exclude=.git . | base64 >> $output
+if base64 --help | grep -q 'GNU coreutils'; then
+  tar cjf - --exclude=.git . | base64 -w 80 >> $output
+else
+  tar cjf - --exclude=.git . | base64 -b 80 >> $output
+fi
