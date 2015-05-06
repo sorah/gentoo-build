23ffc87370b28dad79390d1962ff645c23e89058
Shota Fukumori (sora_h) <sorah@cookpad.com>
2015-05-06 20:02:22 +0900

    implement GB_REMOVE_PORTAGE=1

--- scripts/finalize.sh
+++ scripts/finalize.sh
@@ -9,3 +9,12 @@ set -e
 rm -f /etc/resolv.conf
 ln -snf /run/systemd/resolve/resolv.conf /etc/resolv.conf
 EOF
+
+if [ "_$GB_REMOVE_PORTAGE" = "_1" ]; then
+  chroot ${GB_ROOT} /bin/bash <<-'EOF'
+source /etc/profile
+set -x
+set -e
+rm -rf /usr/portage
+EOF
+fi
