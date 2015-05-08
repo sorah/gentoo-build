32a8cca6845cdb52d55f037245e5cd2f2e5938c5
Shota Fukumori (sora_h) <sorah@cookpad.com>
2015-05-08 17:32:24 +0900

    refactor

--- scripts/grub.sh
+++ scripts/grub.sh
@@ -9,17 +9,18 @@ set -e
 
 emerge --noreplace -v sys-boot/grub
 echo 'GRUB_CMDLINE_LINUX="${GRUB_CMDLINE_LINUX} init=/usr/lib/systemd/systemd"' >> /etc/default/grub
-if grep -q ' /boot ' /proc/mounts; then
-  if grep ' /boot ' /proc/mounts | grep -q rw; then
-    mount -o rw,remount /boot
-  fi
-fi
 EOF
 
 chroot ${GB_ROOT} /bin/bash <<-EOF
 source /etc/profile
 set -x
 set -e
+
+if grep -q ' /boot ' /proc/mounts; then
+  if grep ' /boot ' /proc/mounts | grep -q rw; then
+    mount -o rw,remount /boot
+  fi
+fi
 
 grub2-install --no-floppy /dev/${GB_ROOTDEVICE}
 grub2-mkconfig -o /boot/grub/grub.cfg
