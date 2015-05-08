df149b83d2ac12527dede839b268ad32948b0b42
Shota Fukumori (sora_h) <sorah@cookpad.com>
2015-05-08 17:33:26 +0900

    grub: disable timeouts

--- scripts/grub.sh
+++ scripts/grub.sh
@@ -8,7 +8,6 @@ set -x
 set -e
 
 emerge --noreplace -v sys-boot/grub
-echo 'GRUB_CMDLINE_LINUX="${GRUB_CMDLINE_LINUX} init=/usr/lib/systemd/systemd"' >> /etc/default/grub
 EOF
 
 if [ "_${GB_GRUB_CONSOLE}" = "_1" ]; then
@@ -28,6 +27,13 @@ chroot ${GB_ROOT} /bin/bash <<-'EOF'
 source /etc/profile
 set -x
 set -e
+
+if ! grep -q 'init=/usr/lib/systemd/systemd' /etc/defaults/grub; then
+  echo 'GRUB_DEFAULT=0' >> /etc/default/grub
+  echo 'GRUB_HIDDEN_TIMEOUT=0' >> /etc/default/grub
+  echo 'GRUB_HIDDEN_TIMEOUT_QUIET=true' >> /etc/default/grub
+  echo 'GRUB_CMDLINE_LINUX="${GRUB_CMDLINE_LINUX} init=/usr/lib/systemd/systemd"' >> /etc/default/grub
+fi
 
 if grep -q ' /boot ' /proc/mounts; then
   if grep ' /boot ' /proc/mounts | grep -q rw; then
