9cc3bba417d79fc9f4e13e0eac393f40048cee72
sorah (Shota Fukumori) <her@sorah.jp>
2016-03-19 23:31:10 +0900

    give a hint

--- scripts/fstab.sh
+++ scripts/fstab.sh
@@ -5,6 +5,11 @@ set -e
 echo "/dev/${GB_ROOTDEVICE}${GB_ROOT_PARTITION} / ${GB_ROOT_FSTYPE} defaults,noatime 1 2" > ${GB_ROOT}/etc/fstab
 if [ -n "${GB_BOOT_PARTITION}" -a -n "${GB_BOOT_FSTYPE}" ]; then
   echo "/dev/${GB_ROOTDEVICE}${GB_BOOT_PARTITION} /boot ${GB_BOOT_FSTYPE} defaults,noatime 1 1" >> ${GB_ROOT}/etc/fstab
+
+  if [ "${GB_BOOT_FSTYPE}" = "btrfs" ]; then
+    # hints
+    echo "# /dev/${GB_ROOTDEVICE}${GB_BOOT_PARTITION} /boot ${GB_BOOT_FSTYPE} noatime,discard,ssd,space_cache 1 1" >> ${GB_ROOT}/etc/fstab
+  fi
 fi
 
 if [ -n "${GB_EFI_PARTITION}" ]; then
