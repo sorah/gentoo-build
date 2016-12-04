ae95257d01084632d84f5484838aba74c438405a
Sorah Fukumori <her@sorah.jp>
2016-12-04 10:05:41 +0900

    machine-id is created only when GB_REMOVE_PORTAGE=1

--- scripts/finalize.sh
+++ scripts/finalize.sh
@@ -8,6 +8,7 @@ set -x
 set -e
 rm -f /etc/resolv.conf
 ln -snf /run/systemd/resolve/resolv.conf /etc/resolv.conf
+true > /etc/machine-id
 EOF
 
 if [ "_$GB_REMOVE_PORTAGE" = "_1" ]; then
@@ -17,6 +18,5 @@ set -x
 set -e
 rm -rf /usr/portage
 rm -fv /stage3.tar.bz2
-true > /etc/machine-id
 EOF
 fi
